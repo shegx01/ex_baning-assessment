e1748bf8a07b47b9d7f414911f83c624b83d9a51
O.O.Samson <shegx01@gmail.com>
2022-02-10 19:12:37 +0100

    fix(readme): fixed image link

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -4,6 +4,7 @@ defmodule ExBanking do
 
   @moduledoc """
     Api module that exposed the app functionality
+
   """
 
   @type deposit_withdraw_response ::
@@ -16,7 +17,7 @@ defmodule ExBanking do
 
   @doc """
     - Create new user and adhere to api return type if argument meet the requirement
-    - Empty user string is rejected
+    - Empty user string is rejected as an error
   """
   @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
   def create_user(name) when is_bitstring(name) do
@@ -34,6 +35,9 @@ defmodule ExBanking do
     |> Customer.Producer.create_transaction()
   end
 
+  @doc """
+    withdraw money from user/customer account
+  """
   @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
           {:ok, new_balance :: number} | deposit_withdraw_response()
   def withdraw(user, amount, currency) do
@@ -41,6 +45,9 @@ defmodule ExBanking do
     |> Customer.Producer.create_transaction()
   end
 
+  @doc """
+    get the remaining balance from the user/customer
+  """
   @spec balance(user :: String.t(), currency :: String.t()) ::
           {:ok, balance :: number} | {:error, atom()}
   def balance(user, currency) do
@@ -48,6 +55,10 @@ defmodule ExBanking do
     |> Customer.Producer.create_transaction()
   end
 
+  @doc """
+    send money from one user to the other
+    if user1 is the same as user2, an error is returned
+  """
   @spec send(
           from_user :: String.t(),
           to_user :: String.t(),
